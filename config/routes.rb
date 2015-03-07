e30479308776819126756ecc3fc9a8727a205ac1
Alex <alexandralaudon@gmail.com>
2015-03-07 14:08:33 -0700

    completed rspec spec/features/0_signup_spec.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get 'sign-up', to: 'registrations#new'
+  post 'sign-up', to: 'registrations#create'
+  resources :cheeses, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

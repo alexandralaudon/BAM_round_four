e7c3ecc6969b67bca4c7a9c13fcf3202c37bd120
Alex <alexandralaudon@gmail.com>
2015-03-07 15:04:27 -0700

    Completed rspec spec/features/2_cheeses_crud.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :ensure_current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
+  end
+
+  def ensure_current_user
+    unless current_user
+      flash[:notice] = 'You must register or log in before you can do that!'
+      redirect_to root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post 'sign-up', to: 'registrations#create'
   get 'sign-in', to: 'authentication#new'
   post 'sign-in', to: 'authentication#create'
-  resources :cheeses, only: [:index]
+  resources :cheeses
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
